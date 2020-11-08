ee87dee7d0c444d9c9e803137ebcb871bf52ab81
Mikhail Lisus <lisusmikhail@gmail.com>
2020-11-07 22:28:44 -0500

    first stage media

--- src/components/Media.js
+++ src/components/Media.js
@@ -3,14 +3,18 @@ import Player from './Player.js'
 
 const Media = () => {
 
+  const testFunctions=()=> {
+    console.log('Testing functions')
+  }
+
   return (
     <section className="media">
       <div className="media__player">
         <p className="media__current-track">I Have A Dream</p>
         <Player />
       </div>
-      <button className="media__button-open"/>
-      <button className="media__button-track media__button-track_opened">Текст песни</button>
+      <button className="media__button-open" onClick={testFunctions}/>
+      <button className="media__button-track media__button-track_opened" >Текст песни</button>
     </section>
   )
 }
--- src/components/WindowSize.js
+++ src/components/WindowSize.js
@@ -17,14 +17,11 @@ function useWindowSize() {
         height: window.innerHeight,
       });
     }
-
     // Add event listener
     window.addEventListener("resize", handleResize);
-
     // Call handler right away so state gets updated with initial window size
     handleResize();
-
-    // Remove event listener on cleanup
+1    // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
 
